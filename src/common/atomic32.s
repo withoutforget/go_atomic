c5bbfb4800e0cc5242436444e245130c26835965
Roman Kovach <idcfyoy@gmail.com>
2025-10-31 12:07:19 +0300

    made load\store via cmpchg for seq-cst

--- src/common/atomic32.s
+++ src/common/atomic32.s
@@ -4,14 +4,14 @@
 TEXT ·AtomicStore32(SB), NOSPLIT, $0-12
     MOVQ x+0(FP), AX
     MOVL y+8(FP), BX
-    MOVL BX, (AX)
+    XCHGL BX, (AX)
     RET
 
 // func AtomicLoad32(src *int32) int32
 TEXT ·AtomicLoad32(SB), NOSPLIT, $0-12
     MOVQ x+0(FP), AX
     MOVL (AX), AX
-    MOVL AX, ret+8(FP)
+    XCHGL AX, ret+8(FP)
     RET
 
 // func AtomicAdd32(dst *int32, val int32)
